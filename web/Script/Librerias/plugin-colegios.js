7434b1eb9029179a75a3bc5b4cfef2f420bc0969
Andres Diaz <jaud_@hotmail.com>
2020-04-22 21:07:56 -0500

    formularios tabla y por registro

--- web/Script/Librerias/plugin-colegios.js
+++ web/Script/Librerias/plugin-colegios.js
@@ -42,3 +42,19 @@ function mostrar_panel() {
     }
 
 }
+function selectText(_this) {
+    var doc = document;
+    var element = _this;
+    console.log(_this, element);
+    if (doc.body.createTextRange) {
+        var range = document.body.createTextRange();
+        range.moveToElementText(element);
+        range.select();
+    } else if (window.getSelection) {
+        var selection = window.getSelection();
+        var range = document.createRange();
+        range.selectNodeContents(element);
+        selection.removeAllRanges();
+        selection.addRange(range);
+    }
+}
